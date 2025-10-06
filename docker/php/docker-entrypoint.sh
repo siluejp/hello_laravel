ee89f18fbd4b9ccdd10d19917c7dc782afd39801
suzuki shinji <>
2025-10-06 12:53:48 +0000

    delete sh

--- docker/php/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-if [ ! -f /var/www/html/vendor/autoload.php ]; then
-  echo "vendor/autoload.php not found; running composer install..."
-  composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
-fi
-
-exec "$@"
